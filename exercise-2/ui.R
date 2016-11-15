e6231bf43edbc846849d5071892af3afa2397a73
mckaulay <mckaulay+github@gmail.com>
2016-11-15 14:09:16 -0800

    last week's work

--- exercise-2/ui.R
+++ exercise-2/ui.R
@@ -4,10 +4,12 @@
 shinyUI(fluidPage(
   
   # Add a numbericInput to choose the number of observations
-  
+  numericInput("num", label = h3("Numeric Input"), value = 1),
   
   # Add a selectInput that allows you to select a color from a list of choices
-  
+  selectInput("select", label = h3("Select box"),
+              choices = list("Red" = 'red', "Blue"='blue', "Green" = 'green')
+              slected = 1)
   
   # Plot the output with the name "scatter"
   
